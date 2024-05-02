2cd70e5c326d5840449c7dc3c0baa5df73b89e17
Larissa Soares <larissa.soares@neugelb.com>
2024-05-02 21:42:16 +0200

    feat: add bottom sheet composable

--- app/src/main/java/app/mobiefy/MainActivity.kt
+++ app/src/main/java/app/mobiefy/MainActivity.kt
@@ -5,7 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.res.painterResource
+import app.mobiefy.components.BottomSheet
+import app.mobiefy.components.ButtonWithIcon
 import app.mobiefy.ui.theme.MobiefyTheme
+import app.mobiefy.ui.theme.primary
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +23,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MobiefyTheme {
+                BottomSheet(
+                    ButtonWithIcon(
+                        text = "Button",
+                        icon = painterResource(id = R.drawable.ic_money_circle),
+                        btnColor = primary
+                    )
+                )
             }
         }
     }
