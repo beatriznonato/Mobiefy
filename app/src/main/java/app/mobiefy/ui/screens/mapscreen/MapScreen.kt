6354c7c152df25dcf62cc5aca3feb2cb6c51d490
Larissa Soares <larissa.soares@neugelb.com>
2024-05-03 23:51:46 +0200

    feat: add location pin on map

--- app/src/main/java/app/mobiefy/ui/screens/mapscreen/MapScreen.kt
+++ app/src/main/java/app/mobiefy/ui/screens/mapscreen/MapScreen.kt
@@ -1,15 +1,76 @@
 package app.mobiefy.ui.screens.mapscreen
 
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import app.mobiefy.R
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
 
 @Composable
 fun MapScreen(
 ) {
-    GoogleMap(
-        modifier = Modifier.fillMaxSize()
-    ) {
+    val fiap = LatLng(-23.57305, -46.52056)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(fiap, 9f)
     }
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        MapMarker(
+            position = fiap,
+            title = "FIAP Aclimação",
+            context = LocalContext.current,
+            snippet = "Av. Lins de Vasconcelos, 1222",
+            iconResourceId = R.drawable.ic_location_pin
+        )
+    }
+}
+@Composable
+fun MapMarker(
+    context: Context,
+    position: LatLng,
+    title: String,
+    snippet: String,
+    @DrawableRes iconResourceId: Int
+) {
+    val icon = bitmapDescriptorFromVector(
+        context, iconResourceId
+    )
+    Marker(
+        state = MarkerState(position = position),
+        title = title,
+        icon = icon,
+        snippet = snippet,
+    )
+}
+
+fun bitmapDescriptorFromVector(
+    context: Context,
+    vectorResId: Int
+): BitmapDescriptor? {
+
+    // retrieve the actual drawable
+    val drawable = ContextCompat.getDrawable(context, vectorResId) ?: return null
+    drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+    val bm = Bitmap.createBitmap(
+        drawable.intrinsicWidth,
+        drawable.intrinsicHeight,
+        Bitmap.Config.ARGB_8888
+    )
+
+    // draw it onto the bitmap
+    val canvas = android.graphics.Canvas(bm)
+    drawable.draw(canvas)
+    return BitmapDescriptorFactory.fromBitmap(bm)
 }
