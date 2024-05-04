e6ce83124cfe1872d71200a8aa8b01f6fd21ac96
Larissa Soares <larissa.soares@neugelb.com>
2024-05-04 22:21:55 +0200

    feat: add signup screen

--- app/src/main/java/app/mobiefy/data/Routes.kt
+++ app/src/main/java/app/mobiefy/data/Routes.kt
@@ -3,4 +3,6 @@ package app.mobiefy.data
 sealed class Routes(val route: String) {
     object Welcome : Routes("welcome")
     object Login : Routes("login")
+
+    object SignUp : Routes("signup")
 }
