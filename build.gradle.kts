412207018024ac06b1788b0ae8b263f4f6f434be
Larissa Soares <larissa.soares@neugelb.com>
2024-05-07 22:39:30 +0200

    refactor: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 
-    id("com.google.dagger.hilt.android") version "2.48" apply false
+//    id("com.google.dagger.hilt.android") version "2.48" apply false
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
 
 }
@@ -12,6 +12,5 @@ plugins {
 buildscript {
     dependencies {
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
     }
 }
